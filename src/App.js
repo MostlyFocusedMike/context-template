8830fa6efb8c703b65925359a0b4a44f58db04ef
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-07-29 00:09:59 -0400

    get rough context out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useContext, useState, useEffect } from 'react';
 import './App.css';
+import AppContext from './context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const { example, setExample } = useContext(AppContext);
+    const [formText, setFormText] = useState('');
+
+    useEffect(() => {
+        console.log('context here: ', example);
+    }, [example])
+
+    const handleChange = (e) => {
+        setFormText(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setExample(formText);
+    }
+
+    return (
+        <div className="App">
+            Hello there
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="example">Example: </label>
+                <input
+                    type='text'
+                    value={formText}
+                    onChange={handleChange}
+                />
+                <button>DO IT</button>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
